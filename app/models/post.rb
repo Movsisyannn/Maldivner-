e9f2a91e85a4dd5c938afa09317e16e18b1072e9
Marieta Movsisyan <marietamovsisyan1971@gmail.com>
2024-03-22 13:37:13 -0700

    active_storage

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,15 @@
 class Post < ApplicationRecord
+
+    validates :title,:summary,:body,presence:true
+    	 has_one_attached :image
+
+    	 def thumbnail 
+		return self.image.variant(resize: '100x100!'). 
+		processed
+		
+	end
+	def thumbnail1 
+		return self.image.variant(resize: '150x150!').
+		processed 
+	end
 end
